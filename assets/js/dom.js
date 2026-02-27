407fd1797fbf7e00962f370bb16bae24c970c7b1
Mariam <codegirlbxl@gmail.com>
2026-02-28 00:17:15 +0100

    Add JS to capitalize text

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -5,3 +5,13 @@ document.querySelectorAll(".dropdown > a").forEach((link) => {
     this.parentElement.classList.toggle("open");
   });
 });
+
+/* Selectionne tout les f4 ayant la classe "footert" et transforme la premier lettre en majuscule */
+document.querySelectorAll(".footert").forEach((h4) => {
+  h4.style.textTransform = "capitalize";
+});
+
+/* Selectionne tout les p ayant la classe "footert" et transforme la premier lettre en majuscule */
+document.querySelectorAll(".container p").forEach((p) => {
+  p.style.textTransform = "capitalize";
+});

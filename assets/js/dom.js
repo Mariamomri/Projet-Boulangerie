d3616b1f38d8fd9d55f1e23ca9d9b441a7b8473e
Mariam <codegirlbxl@gmail.com>
2026-02-28 02:37:48 +0100

    add js for carossel slides

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -6,12 +6,30 @@ document.querySelectorAll(".dropdown > a").forEach((link) => {
   });
 });
 
-/* Selectionne tout les f4 ayant la classe "footert" et transforme la premier lettre en majuscule */
+/* metre en majescule le premier lettre de footer */
 document.querySelectorAll(".footert").forEach((h4) => {
   h4.style.textTransform = "capitalize";
 });
-
-/* Selectionne tout les p ayant la classe "footert" et transforme la premier lettre en majuscule */
 document.querySelectorAll(".container p").forEach((p) => {
   p.style.textTransform = "capitalize";
 });
+
+/* Carousel */
+const slides = document.querySelectorAll(".carousel .slide");
+let currentIndex = 0;
+
+function showSlide(index) {
+  currentIndex = (index + slides.length) % slides.length;
+  slides.forEach((s) => s.classList.remove("active"));
+  slides[currentIndex].classList.add("active");
+}
+
+const backicon = document.querySelector(".backicon");
+const nexticon = document.querySelector(".nexticon");
+
+if (backicon) {
+  backicon.addEventListener("click", () => showSlide(currentIndex - 1));
+}
+if (nexticon) {
+  nexticon.addEventListener("click", () => showSlide(currentIndex + 1));
+}
